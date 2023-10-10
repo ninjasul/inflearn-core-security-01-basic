c9d383fbfa7e37737e8e7e7a7fe2a3955940d107
dongyub.park <dongyub.park@kurlycorp.com>
2023-10-10 12:37:38 +0900

    Initial commit

---
+++ src/test/java/io/security/basic/InflearnCoreSecurity01BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnCoreSecurity01BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
